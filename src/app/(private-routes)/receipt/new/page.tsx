c45a69bf555cd528a59e5485a51d2f2f188d8e17
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-04-11 16:33:55 -0300

    fix receipts page

--- src/app/(private-routes)/receipt/new/page.tsx
+++ src/app/(private-routes)/receipt/new/page.tsx
@@ -1,11 +1,14 @@
+import { getReceiptsCategory } from '@/actions'
 import { PageTitle } from '@/components/PageTitle'
 import { ReceiptForm } from '@/components/forms/Receipt'
 
-export default function NewReceiptPage() {
+export default async function NewReceiptPage() {
+  const receiptCategory = await getReceiptsCategory()
+
   return (
     <>
       <PageTitle title="Cadastrar Receita" />
-      <ReceiptForm />
+      <ReceiptForm categories={receiptCategory} />
     </>
   )
 }
