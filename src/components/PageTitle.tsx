4dfa45f9632d732d963e9eda804ea53d709ee933
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-04-12 14:45:18 -0300

    update expense components

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,4 +1,8 @@
-export function PageTitle({ title }: { title: string }) {
+interface Props {
+  title: string
+}
+
+export function PageTitle({ title }: Props) {
   return (
     <h1 className="text-lg text-neutral-600 dark:text-neutral-400">{title}</h1>
   )
--- src/components/renders/expenses/index.tsx
+++ src/components/renders/expenses/index.tsx
@@ -1,12 +1,16 @@
 import { PageTitle } from '@/components/PageTitle'
 import { ExpenseWithCategories } from '@/lib/interfaces'
 import { MonthToString } from '@/utils/monthToString'
+import Link from 'next/link'
 
 interface Props {
   data: ExpenseWithCategories[]
 }
 
 export function RenderExpenseList({ data }: Props) {
+  const currentMonth = new Date().getMonth()
+  const active = (month: number) => month === currentMonth
+
   const groupedExpenses: {
     [key: string]: { [key: string]: ExpenseWithCategories[] }
   } = {}
@@ -29,7 +33,19 @@ export function RenderExpenseList({ data }: Props) {
     return (
       <>
         {data.map((expense) => (
-          <p key={expense.id}>{expense.description}</p>
+          <Link
+            href={`/expense/${expense.id}`}
+            className="my-2 flex items-center justify-between rounded-lg p-2 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+          >
+            <span className="w-1/3">{expense.description}</span>
+            <span className="w-1/3">
+              {expense.value.toLocaleString('pt-br', {
+                currency: 'BRL',
+                style: 'currency',
+              })}
+            </span>
+            <span className="w-1/3">{expense.status}</span>
+          </Link>
         ))}
       </>
     )
@@ -40,12 +56,15 @@ export function RenderExpenseList({ data }: Props) {
       {Object.keys(groupedExpenses).map((month) => (
         <section
           key={month}
-          className="my-2 rounded-lg border border-neutral-500 bg-neutral-100 p-2 dark:bg-neutral-800"
+          className={`my-5 rounded-lg bg-neutral-100 p-2 dark:bg-neutral-800 ${active(parseInt(month)) ? 'border-2 border-neutral-800 shadow dark:border-white' : 'border border-neutral-500'}`}
         >
           <PageTitle title={MonthToString[parseInt(month)].label} />
           {Object.keys(groupedExpenses[month]).map((category) => (
             <div key={category}>
-              <h3>{category}</h3>
+              <h3 className="my-1.5 flex justify-between border-b border-neutral-500 text-sm font-medium text-emerald-700 dark:text-emerald-600">
+                {category}
+                <span>R$ 1.500,90</span>
+              </h3>
               <RenderExpenses data={groupedExpenses[month][category]} />
             </div>
           ))}
